a69185ebb2fa6f8682723059bd4e4c9cc6231bba
Daniel Soares <daniel.soares@me.com>
2020-07-28 14:43:19 -0300

    add GlobalStyles

---
+++ src/components/Layout/global.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: Sans-serif;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 1.5;
+  }
+`
+
+export default GlobalStyles
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,7 +1,11 @@
+import React from 'react'
+
+import GlobalStyles from './global'
 import React from "react"
 
 const Layout = ({ children }) => (
   <>
+    <GlobalStyles />
     <main>{children}</main>
   </>
 )
