93cf2b8c4d8c113c8774099d29035f732480010b
Daniel Soares <daniel.soares@me.com>
2020-07-28 16:58:16 -0300

    add styles

--- src/components/Layout/global.js
+++ src/components/Layout/global.js
@@ -8,10 +8,41 @@ const GlobalStyles = createGlobalStyle`
   }
 
   body {
-    font-family: Sans-serif;
+    font-family: 'Lato', Sans-serif;
     font-size: 16px;
     font-weight: 400;
     line-height: 1.5;
+  }
+
+  a {
+    color: inherit;
+    font-weight: 700;
+    text-decoration: none;
+
+    :hover {
+      text-decoration: underline;
+    }
+  }
+
+  h1, h2, h3 {
+    font-weight: 900;
+    font-style: italic;
+  }
+
+  h1 {
+    margin-bottom: 2rem;
+
+  }
+
+  h2 {
+    font-size: 3.125rem;
+    margin-bottom: 2rem;
+
+  }
+
+  h3 {
+    font-size: 1.25rem;
+    margin-bottom: 1rem;
   }
 `
 
